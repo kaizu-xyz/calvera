76c7e86d275a948bb4fba8c58b8c74add57ce69c
Kaizu <45330362+kaizu-xyz@users.noreply.github.com>
2026-02-25 22:28:04 +0000

    wait strategies

---
+++ src/wait_strategies.rs
@@ -0,0 +1,33 @@
+use std::hint;
+
+use crate::Sequence;
+
+/// Wait strategies are used by consumers when no new events are ready on the ring buffer.
+pub trait WaitStrategy: Copy + Send {
+    /// The wait strategy will wait for the sequence id being available.
+    fn wait_for(&self, sequence: Sequence);
+}
+
+/// Busy spin wait strategy. Lowest possible latency.
+#[derive(Copy, Clone)]
+pub struct BusySpin;
+
+impl WaitStrategy for BusySpin {
+    #[inline]
+    fn wait_for(&self, _sequence: Sequence) {
+        // Do nothing, true busy spin.
+    }
+}
+
+/// Busy spin wait strategy with spin loop hint which enables the processor to optimize its behavior
+/// by e.g. saving power os switching hyper threads. Obviously, this can induce latency.
+///
+/// See also [`BusySpin`].
+#[derive(Copy, Clone)]
+pub struct BusySpinWithSpinLoopHint;
+
+impl WaitStrategy for BusySpinWithSpinLoopHint {
+    fn wait_for(&self, _sequence: Sequence) {
+        hint::spin_loop();
+    }
+}
